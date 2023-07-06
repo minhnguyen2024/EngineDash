3a999e620318be21f05117191646052dfffab99f
Minh T Nguyen <vu508@cummins.com>
2023-07-05 20:48:39 -0400

    minor tailwind styling

--- app/root.tsx
+++ app/root.tsx
@@ -3,8 +3,11 @@ import {
   Link,
   LiveReload,
   Outlet,
+  useLocation
 } from "@remix-run/react";
 import type { LinksFunction } from "@remix-run/node";
+import { useState, useEffect } from "react";
+
 
 import stylesheet from './styles/tailwind.css'
 
@@ -13,6 +16,13 @@ export const links: LinksFunction = () => [
 ]; 
 
 export default function App() {
+  const location = useLocation();
+  const [activeLink, setActiveLink] = useState('');
+
+  useEffect(() => {
+    setActiveLink(location.pathname);
+  }, [location]);
+  console.log("activeLink ", location.pathname)
   return (
     <html lang="en">
       <head>
@@ -25,19 +35,19 @@ export default function App() {
         <Links/>
       </head>
       <body>
-        <nav className="">
+        <nav className=" bg-red-600 text-white font-semibold">
           <ul className="p-6">
-            <li className="inline-block mr-4">
-                <Link to="dashboard">Dashboard</Link>
+            <li className="inline-block mr-4 hover:underline">
+                <Link to="dashboard" className={location.pathname === '/dashboard' ? 'underline font-extrabold' : ''}>Dashboard</Link>
             </li>
-            <li className="inline-block mr-4">
-                <Link to="engine-dash/order">Order Engines</Link>
+            <li className="inline-block mr-4 hover:underline">
+                <Link to="engine-dash/order" className={location.pathname === '/engine-dash/order' ? 'underline font-extrabold' : ''}>Order Engines</Link>
             </li>
-            <li className="inline-block mr-4">
-                <Link to="engine-dash/search">Search for Engines</Link>
+            <li className="inline-block mr-4 hover:underline">
+                <Link to="engine-dash/search" className={location.pathname === '/engine-dash/search' ? 'underline font-extrabold' : ''}>Search for Engines</Link>
             </li>
-            <li className="inline-block mr-4">
-                <Link to="manage-inventory">Manage Inventory</Link>
+            <li className="inline-block mr-4 hover:underline">
+                <Link to="manage-inventory" className={location.pathname === '/manage-inventory' ? 'underline font-extrabold' : ''}>Manage Inventory</Link>
             </li>
           </ul>
         </nav>
